f8aba6ea8fd0e7444c65697b2c72132a9f085207
JohnOlsea <91258573+JohnOlsea@users.noreply.github.com>
2024-03-13 23:10:55 +0700

    updated

--- Software Team Project/src/App.js
+++ Software Team Project/src/App.js
@@ -1,23 +1,119 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react';
+import styled from 'styled-components';
+
+const StyledContainer = styled.div`
+  display: grid;
+`;
 
 function App() {
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.src = 'https://apis.google.com/js/platform.js';
+    script.async = true;
+    script.defer = true;
+
+    // Add an onerror handler to catch loading errors
+    script.onerror = (error) => {
+      console.error('Error loading Google API script:', error);
+    };
+
+    document.head.appendChild(script);
+
+    script.onload = () => {
+      window.gapi.load('auth2', () => {
+        window.gapi.auth2.init({
+          client_id: 'YOUR_GOOGLE_CLIENT_ID',
+        });
+      });
+    };
+  }, []);
+
+  const handleSignInWithGoogle = () => {
+    const auth2 = window.gapi.auth2.getAuthInstance();
+    auth2.signIn().then((googleUser) => {
+      const profile = googleUser.getBasicProfile();
+      console.log('ID: ' + profile.getId());
+      console.log('Full Name: ' + profile.getName());
+      console.log('Given Name: ' + profile.getGivenName());
+      console.log('Family Name: ' + profile.getFamilyName());
+      console.log('Image URL: ' + profile.getImageUrl());
+      console.log('Email: ' + profile.getEmail());
+      // You can now use the user information as needed
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ display: 'flex', alignItems: 'center' }}>
+      <div style={{ marginLeft: '200px' }}>
+        <StyledContainer>
+          <h4
+            style={{
+              textAlign: 'left',
+              marginBottom: '8px',
+              fontFamily: 'Lato',
+            }}
+          >
+            Email
+          </h4>
+          <input
+            type="text"
+            id="username"
+            style={{
+              width: '395px',
+              height: '40px',
+              background: '#e0e0e0',
+              border:'1px solid black',
+            }}
+          />
+          <h4
+            style={{
+              textAlign: 'left',
+              marginBottom: '8px',
+              fontFamily: 'Lato',
+            }}
+          >
+            Password
+          </h4>
+          <input
+            type="password"
+            id="password"
+            style={{
+              width: '395px',
+              height: '40px',
+              background: '#e0e0e0',
+              border:'1px solid black',
+              marginBottom: '20px',
+            }}
+          />
+          <button className="login-button" onClick={() => {}}>
+            Log in
+          </button>
+          <h4
+            style={{
+              textAlign: 'center',
+              marginBottom: '2px',
+              fontFamily: 'Lato',
+            }}
+          >
+            OR
+          </h4>
+          <h5
+            style={{
+              textAlign: 'center',
+              marginBottom: '8px',
+              fontFamily: 'Lato',
+            }}
+          >
+            Verify with  KMITL email
+          </h5>
+          <button
+            className="google-signin-button"
+            onClick={handleSignInWithGoogle}
+          >
+            Sign In With Google
+          </button>
+        </StyledContainer>
+      </div>
     </div>
   );
 }
---
+++ Software Team Project/src/HomePage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import styled from 'styled-components';
+
+function HomePage() {
+
+
+  return (
+    <div style={{ display: 'flex', alignItems: 'center' }}>
+    </div>
+  );
+}
+
+export default HomePage;

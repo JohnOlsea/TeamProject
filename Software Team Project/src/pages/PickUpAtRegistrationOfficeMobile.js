f9b9fe3bc8b9858832510df5b09c85cd0ad9819b
TRP64011655 <90514870+TRP64011655@users.noreply.github.com>
2024-04-08 00:14:06 +0700

    update css

--- Software Team Project/src/pages/PickUpAtRegistrationOfficeMobile.js
+++ Software Team Project/src/pages/PickUpAtRegistrationOfficeMobile.js
@@ -42,7 +42,7 @@ function PickUpatRegistrationOfficeMobile() {
         <p className="pc-announcement-title">🎓 Pick Up at Registration Office 🎓</p>
         <img src={kmitlRegisOfficeImage} alt="Graduation Image" className="grad-image" />
 
-        <p className="pc-description">Congratulations on your graduation achievement!</p>
+        <p className="pc-description2">🎉 Congratulation 🎉</p>
         <p className="pc-description">Opting to pick up your degree certificate at the registration office during our regular business hours is a smart choice for those with busy schedules.</p>
         <p className="pc-description">We understand the demands of balancing academics and other commitments, and we're here to make the process as convenient as possible for you. </p>
         <p className="pc-description">Our office is open from 9:00 - 17:00</p>

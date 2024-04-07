dc5c7100efc4b0808d97cd046fb77ac430e34770
JohnOlsea <91258573+JohnOlsea@users.noreply.github.com>
2024-04-07 23:02:01 +0700

    update4

--- Software Team Project/src/pages/Login.js
+++ Software Team Project/src/pages/Login.js
@@ -17,14 +17,14 @@ export const ErrorMessage = styled.p`
 `;
 function Login() {
 
-  const getUser = async()=>{
-    try {
-      const response =  await axios.get("http://localhost:5000/login/success", {withCredentials:  true})
-      navigate('/home')
-    } catch (err) {
-      console.log(err);
-      navigate('/');
-    }
+  const getUser = ()=>{
+    // try {
+    //   const response =  await axios.get("http://localhost:5000/login/success", {withCredentials:  true})
+    //   navigate('/home')
+    // } catch (err) {
+    //   console.log(err);
+    //   navigate('/');
+    // }
   }
 
   useEffect(() => {

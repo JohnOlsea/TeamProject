18a4a488e2262f9b92b774cbe0a0dfbc98558392
TRP64011655 <90514870+TRP64011655@users.noreply.github.com>
2024-05-25 16:59:12 +0700

    deleted unnecessary files

--- Software Team Project/src/App.js
+++ Software Team Project/src/App.js
@@ -5,12 +5,9 @@ import React, { useState } from 'react';
 import Home from './pages/Home'
 import PersonalInfo from './pages/PersonalInfo'
 import Login from './pages/Login'
-import DegreeCertificateCollection from './pages/DegreeCertificateCollection'
 import AddressConfirmation from './pages/AddressConfirmation'
 import Payment from './pages/Payment'
 import PaymentCompleted from './pages/PaymentCompleted'
-import GraduationDayPickup from './pages/GraduationDayPickUp'
-import PickUpatRegistrationOffice from './pages/PickUpAtRegistrationOffice'
 import SeeYourOption from './pages/SeeYourOption.js'
 import ChangeReceipt from './pages/ChangeReceipt.js'
 import PaymentForGraduaPick from './pages/PaymentGra_Pick.js'
@@ -43,12 +40,9 @@ function App() {
           <Route path='login' element={<Login />} />
           <Route path='home' element={<Home />} />
           <Route path='personalInfo' element={<PersonalInfo />} />
-          <Route path='degreeCertificateCollection' element={<DegreeCertificateCollection />} />
           <Route path='addressConfirmation' element={<AddressConfirmation />} />
           <Route path='payment' element={<Payment />} />
           <Route path='paymentCompleted' element={<PaymentCompleted />} />
-          <Route path='graduationDayPickup' element={<GraduationDayPickup />} />
-          <Route path='pickUpatRegistrationOffice' element={<PickUpatRegistrationOffice />} />
           <Route path='seeyouroption' element={<SeeYourOption />} />
           <Route path='changeReceipt' element={<ChangeReceipt />} />
           <Route path='paymentNondelivery' element={<PaymentForGraduaPick />} />
